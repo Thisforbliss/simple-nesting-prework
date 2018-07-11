6d6ab1f8dbd3812f59598954bbaf15cc78350bd1
Malcolm Leake <echelon1089@gmail.com>
2018-07-11 02:47:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,9 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
-end
-
+     programmer_hash[:dennis_ritchie][:languages.join()]
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
